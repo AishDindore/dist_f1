af27b1b9115929462b73c019aaccbd1f9fb9e5f8
python_practice <aishwarya.dindore07@gmail.com>
2024-11-27 22:11:03 +0530

    slice

---
+++ str_slice.py
@@ -0,0 +1,29 @@
+name = "chinmay"
+#            0         1         2          3        4      5 
+names  = ["chinmay","sarika","shraddha","abhisha","ninad","mohan"]
+#           -6        -5         -4         -3         -2       -1
+
+e1=names[1:4]
+print(e1)
+
+e2=names[:4]
+print(e2)
+
+e3=names[3:]
+print(e3)
+
+e4=names[-5:]
+print(e4)
+
+e5=names[-6:-2]
+print(e5)
+
+print(names[:len(names)])
+
+print(names[::3])
+
+print(names[::2])
+
+print(names[::-2])
+
+print(names[::-1])
