3a2aa380305284017502c0144a0cb35e39040251
python_practice <aishwarya.dindore07@gmail.com>
2024-11-29 21:51:15 +0530

    str_method

---
+++ str_method.py
@@ -0,0 +1,30 @@
+fn="AISHWARYA"
+print(fn.upper())
+print(fn.lower())
+print(fn.capitalize())
+print(fn.swapcase())
+
+info = "I am learning js"
+print(info.title())
+
+ln="joshi"
+print(ln.center(8,"*"))
+print(ln.ljust(15,"*"))
+print(ln.rjust(15,"*"))
+
+text = "42"
+t = text.zfill(10)
+print(t)
+
+#print 0 to the left side
+t1="chinmay"
+t1 = t1.zfill(10)
+print(t1)
+
+t2="aishwarya"      # count of a letter...3
+t2=t2.count('a')
+print(t2)
+
+text = "chinmay"   #find index value of a.....5
+r = text.find('a')
+print(r)
